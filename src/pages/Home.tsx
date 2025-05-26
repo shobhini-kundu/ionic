bd18e1ea2d6a93df7cca7b7b2c38144d193bae5e
shobhini-kundu <shobhinikundu@gmail.com>
2025-05-26 18:12:01 +0530

    initial code setup done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,47 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { info } from './array';
 
 const Home: React.FC = () => {
+  info.map((name)=>{console.log(<tr><td>name</td></tr>)})
+  
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+  <>
+  <table>
+    <thead><tr>
+      <th>name</th>
+      <th>last name
+      </th>
+      <th>age</th>
+    </tr>
+    </thead>
+    <tbody>
+      <tr>
+      <td>ram</td>
+      <td>kumar</td>      
+      <td>41</td>
+    </tr>
+     <tr>
+      <td>ram</td>
+      <td>kumar</td>      
+      <td>42</td>
+    </tr>
+    <tr>
+       <td>ram</td>
+      <td>kumar</td>      
+      <td>43</td>
+    </tr>
+     <tr>
+       <td>ram</td>
+      <td>kumar</td>      
+      <td>44</td>
+    </tr>
+    </tbody>
+    
+  </table>
+  <div></div>
+  </>
   );
 };
 
---
+++ src/pages/array.ts
@@ -0,0 +1,6 @@
+export const info=[
+    { name:"Ram", lastName:"Kumar", age:41 },
+    {name:"Ram", lastName:"Kumar", age:42},
+    { name:"Ram", lastName:"Kumar", age:43},
+    {name:"Ram", lastName:"Kumar", age:44}
+]
